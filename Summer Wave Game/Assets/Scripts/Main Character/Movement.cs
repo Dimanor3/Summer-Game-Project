58f06ee2af71411f95345d0d1d02194d52b78e88
Dimanor3 <bijanrazavi15@yahoo.com>
2016-05-28 15:20:16 -0400

    Added some basic movement, no rotation yet

---
+++ Summer Wave Game/Assets/Scripts/Main Character/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float rotation;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10f;
+		rotation = 10f;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+			rb.AddForce(new Vector3(-1f * moveSpeed, 0f, 0f));
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			rb.AddForce(new Vector3(1f * moveSpeed, 0f, 0f));
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			rb.AddForce(new Vector3(0f, 0f, 1f * moveSpeed));
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			rb.AddForce(new Vector3(0f, 0f, -1f * moveSpeed));
+		}
+
+	}
+}
