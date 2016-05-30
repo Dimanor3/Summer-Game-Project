00a403b4973ceeae2b57d6b0b4a650d83efc2716
Dimanor3 <bijanrazavi15@yahoo.com>
2016-05-30 09:41:14 -0400

    Movement, Rotation and Camera Rotation finally work beautifully!

--- Summer Wave Game/Assets/Scripts/Main Character/CameraRotation.cs
+++ Summer Wave Game/Assets/Scripts/Main Character/CameraRotation.cs
@@ -12,8 +12,6 @@ public class CameraRotation : MonoBehaviour {
 	// Reset the cameras rotation after free rotation is
 	// deactivated
 	[SerializeField] private float camX;
-	[SerializeField] private float camY;
-	[SerializeField] private float camZ;
 
 	// Free rotation keycode
 	private KeyCode freeRotation;
@@ -33,11 +31,9 @@ public class CameraRotation : MonoBehaviour {
 		// Initialize the camera's natural rotation
 		// (not supposed to change)
 		camX = 22f;
-		camY = 0f;
-		camZ = 0f;
 
 		// Initialize resetCam
-		resetCam = new Vector3(camX, camY, camZ);
+		resetCam = new Vector3(camX, 0f, 0f);
 
 		// Initialize freeRotation
 		freeRotation = KeyCode.Mouse1;
@@ -63,7 +59,8 @@ public class CameraRotation : MonoBehaviour {
 
 		// Reset camera rotation after free rotation is released
 		if(Input.GetKeyUp(freeRotation)){
-			Camera.main.transform.localRotation = Quaternion.Euler(new Vector3(camX, camY, camZ));
+			Camera.main.transform.localRotation = Quaternion.Euler(resetCam);
 		}
 	}
+
 }
--- Summer Wave Game/Assets/Scripts/Main Character/PlayerMotor.cs
+++ Summer Wave Game/Assets/Scripts/Main Character/PlayerMotor.cs
@@ -1,18 +1,57 @@
 ﻿using UnityEngine;
-using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour {
 
-	// Free rotation keycode
-	private KeyCode freeRotation;
+	// Access to the main characters rigid body
+	private Rigidbody rb;
+
+	// Move main character
+	private Vector3 movement;
+
+	// Rotate main character
+	private Vector3 rotation;
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		// Initialize rigid body
+		rb = GetComponent<Rigidbody>();
+
+		// Initializes movement
+		movement = Vector3.zero;
+
+		// Initializes rotation
+		rotation = Vector3.zero;
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	// Run every physics iteration
+	void FixedUpdate(){
+		PerformMovement();
+		PerformRotation();
 	}
+
+	// Gets a movement vector
+	public void Move(Vector3 move){
+		movement = move;
+	}
+
+	// Gets a rotation vector
+	public void Rotate(Vector3 rot){
+		rotation = rot;
+	}
+
+	// Perform movement
+	void PerformMovement(){
+		if(movement != Vector3.zero){
+			rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+		}
+	}
+
+	// Perform rotation
+	void PerformRotation(){
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+	}
+
 }
