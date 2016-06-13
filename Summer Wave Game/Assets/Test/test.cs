b1a24aaa24bad111eb6c5e42a543fc02f707aa1a
Dimanor3 <bijanrazavi15@yahoo.com>
2016-06-13 00:33:49 -0400

    progress

---
+++ Summer Wave Game/Assets/Test/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	EZObjectPools.EZObjectPool pools;
+	[SerializeField] private GameObject gO;
+
+	// Use this for initialization
+	void Start () {
+		pools = EZObjectPools.EZObjectPool.CreateObjectPool(gO, "Test", 3, false, true, false);
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			if(pools.TryGetNextObject(Vector3.zero, new Quaternion(0f, 0f, 0f, 0f), out gO)){
+				pools.TryGetNextObject(Vector3.zero, new Quaternion(0f,0f,0f,0f));
+			}
+		}
+	}
+}
