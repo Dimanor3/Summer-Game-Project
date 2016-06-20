fd5634b3b8cc9c57be534b57f68699efdbc0c932
Brandon Fitzpatrick <Brandon Fitzpatrick>
2016-06-20 09:15:29 -0400

    AI

---
+++ Summer Wave Game/Assets/Scripts/Enemy/EnemyDamage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour {
+
+	// Base damage attack does
+	[SerializeField] private int baseDmg;
+
+	// New damage attack does after level up
+	[SerializeField] private int newDmg;
+
+	// The constant level modifier (occurs every level)
+	[SerializeField] private int constLvlMod;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Summer Wave Game/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+
+	public float fpsTargetDistance;
+	public float enemyLookDistance;
+	public float attackDistance;
+	public float enemyMovementSpeed;
+	public float damping;
+	public Transform fpsTarget;
+	Rigidbody theRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		theRigidbody = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		fpsTargetDistance = Vector3.Distance (fpsTarget.position, transform.position);
+		if(fpsTargetDistance < enemyLookDistance){
+			lookAtPlayer ();
+
+		}
+		if (fpsTargetDistance < attackDistance) {
+			baseAttack ();
+		}
+	}
+
+	void baseAttack (){
+		theRigidbody.velocity = Vector3.zero;
+		theRigidbody.angularVelocity = Vector3.zero;
+		//theRigidbody.position = transform.position;
+
+	}
+	void lookAtPlayer (){
+		Quaternion rotation = Quaternion.LookRotation (fpsTarget.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime*damping);
+		theRigidbody.AddForce (transform.forward*enemyMovementSpeed);
+
+	}
+}
