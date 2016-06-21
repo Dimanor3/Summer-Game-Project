b717c8a35605d79566a170c875d56b13169c0139
Dimanor3 <bijanrazavi15@yahoo.com>
2016-06-21 11:44:17 -0400

    progress

--- Summer Wave Game/Assets/Scripts/Switch Scenes/MainMenu.cs
+++ Summer Wave Game/Assets/Scripts/Switch Scenes/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
-		if(gameObject.GetComponent<PlayerController>().getHealth() <= 0){
+		if(gameObject.GetComponent<PlayerController>().getHealth() <= 0 || gameObject.transform.position.y <= -10f){
 			SceneManager.LoadScene(0);
 		}
 	}
